3d9fc3774e2032474b5746ad4aea2cda4f9835be
Vaki-mr1 <ivangomel2112@gmail.com>
2025-12-11 23:58:46 +0300

    Update vector_ivan_2.0.cpp

--- vector_ivan_2.0.cpp
+++ vector_ivan_2.0.cpp
@@ -1,152 +1,224 @@
-#include <iostream>
+ #include <iostream>
+#include <random>
+#include <chrono>
+#include <algorithm> // для std::min
 
-template <typename T>
-class Subvector {
-private:
-    T* dataArr;
-    unsigned int sz;
-    unsigned int cap;
-
-    void copyFrom(const Subvector& src) {
-        dataArr = new T[src.cap];
-        for (unsigned int i = 0; i < src.sz; i++) {
-            dataArr[i] = src.dataArr[i];
-        }
-        sz = src.sz;
-        cap = src.cap;
-    }
-
-public:
-    Subvector() : dataArr(nullptr), sz(0), cap(0) {}
-
-    explicit Subvector(unsigned int c) {
-        dataArr = new T[c];
-        sz = 0;
-        cap = c;
-    }
-
-    ~Subvector() {
-        delete[] dataArr;
-    }
-
-    Subvector(const Subvector& other) {
-        copyFrom(other);
-    }
-
-    Subvector(Subvector&& other) {
-        dataArr = other.dataArr;
-        sz = other.sz;
-        cap = other.cap;
-
-        other.dataArr = nullptr;
-        other.sz = 0;
-        other.cap = 0;
-    }
-
-    Subvector& operator=(const Subvector& other) {
-        if (this != &other) {
-            delete[] dataArr;
-            copyFrom(other);
-        }
-        return *this;
-    }
-
-    Subvector& operator=(Subvector&& other) {
-        if (this != &other) {
-            delete[] dataArr;
-            dataArr = other.dataArr;
-            sz = other.sz;
-            cap = other.cap;
-
-            other.dataArr = nullptr;
-            other.sz = 0;
-            other.cap = 0;
-        }
-        return *this;
-    }
-
-    bool push_back(const T& value) {
-        if (sz >= cap) {
-            unsigned int newCap = (cap == 0 ? 2 : cap * 2);
-
-            T* tmp = new T[newCap];
-            for (unsigned int i = 0; i < sz; i++) {
-                tmp[i] = dataArr[i];
-            }
-
-            delete[] dataArr;
-            dataArr = tmp;
-            cap = newCap;
-        }
-
-        dataArr[sz++] = value;
-        return true;
-    }
-
-    T pop_back() {
-        if (sz == 0)
-            return T{};
-        sz--;
-        return dataArr[sz];
-    }
-
-    bool resize(unsigned int newCap) {
-        if (newCap == cap)
-            return true;
-
-        T* temp = new T[newCap];
-        unsigned int m = (sz < newCap ? sz : newCap);
-
-        for (unsigned int i = 0; i < m; i++) {
-            temp[i] = dataArr[i];
-        }
-
-        delete[] dataArr;
-        dataArr = temp;
-        cap = newCap;
-        sz = m;
-
-        return true;
-    }
-
-    void shrink_to_fit() {
-        if (sz == cap)
-            return;
-
-        if (sz == 0) {
-            delete[] dataArr;
-            dataArr = nullptr;
-            cap = 0;
-            return;
-        }
-
-        T* tmp = new T[sz];
-        for (unsigned int i = 0; i < sz; i++) {
-            tmp[i] = dataArr[i];
-        }
-
-        delete[] dataArr;
-        dataArr = tmp;
-        cap = sz;
-    }
-
-    void clear() {
-        sz = 0;
-    }
-
-    T& operator[](unsigned int idx) {
-        return dataArr[idx];
-    }
-
-    const T& operator[](unsigned int idx) const {
-        return dataArr[idx];
-    }
-
-    unsigned int size() const {
-        return sz;
-    }
-
-    unsigned int get_capacity() const {
-        return cap;
-    }
+struct subvector {
+     int *mas;
+     unsigned int top; 
+     unsigned int capacity;
 };
+
+bool init(subvector *qv) {
+    qv->mas = NULL;
+    qv->top = 0;
+    qv->capacity = 0;
+    return true;
+}
+
+bool resize(subvector *qv, unsigned int new_capacity) {
+    if (new_capacity == 0) {
+        delete[] qv->mas;
+        qv->mas = NULL;
+        qv->top = 0;
+        qv->capacity = 0;
+        return true;
+    }
+
+    int *new_mas = new int[new_capacity];
+    
+    unsigned int n = qv->top;
+    if (new_capacity < n) {
+        n = new_capacity;
+    }
+
+    for (unsigned int i = 0; i < n; i++) {
+        new_mas[i] = qv->mas[i];
+    }
+
+    delete[] qv->mas; // Удаляем старое
+    qv->mas = new_mas;
+    qv->capacity = new_capacity;
+    
+    if (qv->top > new_capacity) {
+        qv->top = new_capacity;
+    }
+
+    return true;
+}
+
+bool push_back(subvector *qv, int d) {
+    if (qv->top >= qv->capacity) {
+        unsigned int new_cap = qv->capacity * 2;
+        if (new_cap == 0) new_cap = 1;
+        
+        if (!resize(qv, new_cap)) {
+            return false;
+        }
+    }
+
+    qv->mas[qv->top] = d;
+    qv->top++;
+    return true;
+}
+
+// Удаление элемента
+int pop_back(subvector *qv) {
+    if (qv->top == 0) {
+        return 0;
+    }
+    qv->top--;
+    return qv->mas[qv->top];
+}
+
+void shrink_to_fit(subvector *qv) {
+    if (qv->capacity > qv->top) {
+        resize(qv, qv->top);
+    }
+}
+
+void clear(subvector *qv) {
+    qv->top = 0;
+}
+
+void destructor(subvector *qv) {
+    delete[] qv->mas;
+    qv->mas = NULL;
+    qv->top = 0;
+    qv->capacity = 0;
+}
+
+
+
+
+using std::cout;
+using std::endl;
+double get_time()
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count()/1e6;
+}
+int rand_uns(int min, int max)
+{
+        unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+        static std::default_random_engine e(seed);
+        std::uniform_int_distribution<int> d(min, max);
+        return d(e);
+}
+int main()
+{
+    int n = 100000;
+    int *test_sequence = new int[n], sum_for_O3 = 0, sum_check = 0,
+        *pop_push_sequence_eq = new int[n],
+        *pop_push_sequence_push = new int[n],
+        *pop_push_sequence_pushpush = new int[n];
+    double start = 0, finish = 0, total = 0;
+    cout << std::fixed;
+    cout.precision(4);
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        test_sequence[i] = rand_uns(0, n - 1);
+        pop_push_sequence_eq[i] = rand_uns(0, 1);
+        pop_push_sequence_push[i] = rand_uns(0, 5);
+        pop_push_sequence_pushpush[i] = rand_uns(0, 10);
+    }
+    finish = get_time();
+    cout << "Test sequence initialization: \t\t" << finish - start << endl;
+    subvector sv;
+    init(&sv);
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        push_back(&sv, test_sequence[i]);
+    }
+    finish = get_time();
+    for (int i = 0; i < n; i++)   //!!! This is a hack to bamboozle the O3 optimization.
+        sum_for_O3 += sv.mas[i];  // I might as well use it to test push/pop.
+    cout << "000 Straight push_back: \t\t" << finish - start << endl;
+    total += finish - start;
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        sum_check += pop_back(&sv);
+    }
+    finish = get_time();
+    if (sum_check != sum_for_O3)
+    {
+        cout <<endl <<"--- !!! Failed push/pop consistency !!! ---" << endl;
+        return 0;
+    }
+    cout << "001 Straight pop_back: \t\t\t" << finish - start << endl;
+    total += finish - start;
+
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        resize(&sv, i);
+    }
+    finish = get_time();
+    shrink_to_fit(&sv);
+    if (sv.capacity)
+    {
+        cout <<endl <<"--- !!! Failed resize/shrink consistency !!! ---" << endl;
+        return 0;
+    }
+    cout << "002 Straight resize up: \t\t" << finish - start << endl;
+    total += finish - start;
+    sum_for_O3 = 0; // This variable will be printed so O3 won't cut the whole thing.
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        if (pop_push_sequence_eq[i])
+            push_back(&sv, test_sequence[i]);
+        else
+            sum_for_O3 += pop_back(&sv);
+    }
+    finish = get_time();
+    clear(&sv);
+    shrink_to_fit(&sv);
+    if (sv.top)
+    {
+        cout <<endl <<"--- !!! Falied clear !!! ---" << endl;
+        return 0;
+    }
+    if (sv.capacity)
+    {
+        cout <<endl <<"--- !!! Falied shrink_to_fit !!! ---" << endl;
+        return 0;
+    }
+    cout << "003 Random pop/push equal amount: \t" << finish - start << "\t\t" << sum_for_O3 << endl;
+    total += finish - start;
+    sum_for_O3 = 0; // This variable will be printed so O3 won't cut the whole thing.
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        if (pop_push_sequence_push[i])
+            push_back(&sv, test_sequence[i]);
+        else
+            sum_for_O3 += pop_back(&sv);
+    }
+    finish = get_time();
+    clear(&sv);
+    shrink_to_fit(&sv);
+    cout << "004 Random pop/push more push: \t\t" << finish - start << "\t\t" << sum_for_O3 << endl;
+    total += finish - start;
+    sum_for_O3 = 0; // This variable will be printed so O3 won't cut the whole thing.
+    start = get_time();
+    for (int i = 0; i < n; i++)
+    {
+        if (pop_push_sequence_pushpush[i])
+            push_back(&sv, test_sequence[i]);
+        else
+            sum_for_O3 += pop_back(&sv);
+    }
+    finish = get_time();
+    cout << "005 Random pop/push much more push: \t" << finish - start << "\t\t" << sum_for_O3 << endl;
+    total += finish - start;
+    destructor(&sv);
+    cout << "-----------" << endl <<"Alltests finished, total time: \t" << total << endl;
+    delete[] test_sequence;
+    delete[] pop_push_sequence_eq;
+    delete[] pop_push_sequence_push;
+    delete[] pop_push_sequence_pushpush;
+    return 0;
+}
